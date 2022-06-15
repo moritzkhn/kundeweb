f9dc68a46830fd6d5cfa4ee08b84abaaf1a6392b
Amy-hgn <76444951+Amy-hgn@users.noreply.github.com>
2022-06-15 15:39:04 +0200

    erstellen

---
+++ src/app/kunde/suche-kunden/suche-kunden.component.ts
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2015 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import {
+    type Kunde,
+    KundeReadService,
+    FindError,
+    type Suchkriterien,
+} from '../shared';
+import { Component, type OnInit } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
+import { HttpStatusCode } from '@angular/common/http';
+import { Title } from '@angular/platform-browser'; // eslint-disable-line @typescript-eslint/consistent-type-imports
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r das Tag <code>&lt;hs-suche-kunden&gt;</code>, die aus
+ * den Kindkomponenten f&uuml;r diese Tags besteht:
+ * <ul>
+ *  <li> <code>hs-suchformular</code>
+ *  <li> <code>hs-waiting</code>
+ *  <li> <code>hs-suchergebnis</code>
+ * </ul>
+ */
+@Component({
+    selector: 'hs-suche-kunden',
+    templateUrl: './suche-kunden.component.html',
+})
+export class SucheKundenComponent implements OnInit {
+    waiting = false;
+
+    kunden: Kunde[] = [];
+
+    errorMsg: string | undefined;
+
+    // Parameter Properties (Empfehlung: Konstruktor nur fuer DI)
+    constructor(
+        private readonly service: KundeReadService,
+        private readonly titleService: Title,
+    ) {
+        log.debug('SucheKundenComponent.constructor()');
+    }
+
+    // Wird von Angular aufgerufen, wenn der DOM-Baum fertig ist,
+    // d.h. nach dem "Rendering".
+    // Wird immer generiert, wenn Angular-CLI genutzt wird.
+    ngOnInit() {
+        this.titleService.setTitle('Suche');
+    }
+
+    /**
+     * Das Attribut <code>suchkriterien</code> wird auf den Wert des Ereignisses
+     * <code>suchkriterien</code> vom Typ Suchkriterien gesetzt. Diese Methode
+     * wird aufgerufen, wenn in der Kindkomponente f&uuml;r
+     * <code>hs-suchformular</code> das Ereignis ausgel&ouml;st wird.
+     *
+     * @param suchkriterien f&uuml;r die Suche.
+     */
+    suchen(suchkriterien: Suchkriterien) {
+        log.debug(
+            'SucheKundenComponent.suchen: suchkriterien=',
+            suchkriterien,
+        );
+
+        this.kunden = [];
+        this.errorMsg = undefined;
+
+        this.waiting = true;
+
+        // Observable: mehrere Werte werden "lazy" bereitgestellt, statt in einem JSON-Array
+        // pipe ist eine "pure" Funktion, die ein Observable in ein NEUES Observable transformiert
+        this.service
+            .find(suchkriterien) // eslint-disable-line unicorn/no-array-callback-reference
+            .pipe(
+                first(),
+                tap(result => this.#setProps(result)),
+            )
+            .subscribe();
+    }
+
+    #setProps(result: Kunde[] | FindError) {
+        this.waiting = false;
+
+        if (result instanceof FindError) {
+            this.#handleFindError(result);
+            return;
+        }
+
+        this.kunden = result;
+        log.debug('SucheKundenComponent.#setProps: kunden=', this.kunden);
+    }
+
+    #handleFindError(err: FindError) {
+        const { statuscode } = err;
+        log.debug(
+            'SucheKundenComponent.#handleError: statuscode=',
+            statuscode,
+        );
+
+        switch (statuscode) {
+            case HttpStatusCode.NotFound:
+                this.errorMsg = 'Keine Kunden gefunden.';
+                break;
+            case HttpStatusCode.TooManyRequests:
+                this.errorMsg =
+                    'Zu viele Anfragen. Bitte versuchen Sie es später noch einmal.';
+                break;
+            case HttpStatusCode.GatewayTimeout:
+                this.errorMsg = 'Ein interner Fehler ist aufgetreten.';
+                log.error('Laeuft der Appserver? Port-Forwarding?');
+                break;
+            default:
+                this.errorMsg = 'Ein unbekannter Fehler ist aufgetreten.';
+                break;
+        }
+
+        log.debug(
+            'SucheKundenComponent.#setErrorMsg: errorMsg=',
+            this.errorMsg,
+        );
+    }
+}
