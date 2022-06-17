4452c5920dc939778c9777e3385eb358e78a5cf9
deal1020 <103117035+deal1020@users.noreply.github.com>
2022-06-17 15:23:51 +0200

    Add files via upload

---
+++ src/app/kunde/suche-kunde/suchformular/suche-nachname.component.ts
@@ -0,0 +1,25 @@
+import { Component, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r das Tag <code>hs-suche-titel</code>
+ */
+@Component({
+    selector: 'hs-suche-nachname',
+    templateUrl: './suche-nachname.component.html',
+})
+export class SucheNachnameComponent {
+    nachname = '';
+    @Output()
+    readonly nachname$ = new Subject<string>();
+
+    constructor() {
+        log.debug('SucheNachnameComponent.constructor()');
+    }
+
+    onBlur() {
+        log.debug(`SucheNachnameComponent.onBlur: nachname=${this.nachname}`);
+        this.nachname$.next(this.nachname);
+    }
+}
